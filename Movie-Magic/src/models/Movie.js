df90949a766663513c511714c28d600d0876d1a2
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-06-02 16:21:26 +0300

    Create mongoose model view

--- Movie-Magic/src/models/Movie.js
+++ Movie-Magic/src/models/Movie.js
@@ -1,23 +1,39 @@
-import fs from 'node:fs/promises'
-import{log} from 'node:console'
-import {v4 as uuid} from 'uuid'
+import {Schema,model} from "mongoose";
 
-const movieJson= await fs.readFile('./src/database.json', 'utf-8')
-export const movies= JSON.parse(movieJson)
+const movieSchema = new Schema({
+    title:String,
+    category:String,
+    genre: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating:Number,
+    description: String,
+})
+
+ const Movie = model('Movie', movieSchema);
+export default Movie;
+
+// import fs from 'node:fs/promises'
+// import{log} from 'node:console'
+// import {v4 as uuid} from 'uuid'
+
+// const movieJson= await fs.readFile('./src/database.json', 'utf-8')
+// export const movies= JSON.parse(movieJson)
 
 
 
-export default class Movie{
-    constructor(data){
-        this.data = data;
-    }
+// export default class Movie{
+//     constructor(data){
+//         this.data = data;
+//     }
    
-    async save(){
+//     async save(){
      
     
-        movies.push(this.data);
-        await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
-        return this.data;
-    }
+//         movies.push(this.data);
+//         await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
+//         return this.data;
+//     }
 
-}
+// }
