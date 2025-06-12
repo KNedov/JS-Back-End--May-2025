1dbe8c24d798f732f1097c8a71c84035af3e5a45
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-06-12 21:08:49 +0300

    Add login register views

--- Movie-Magic/src/controllers/userController.js
+++ Movie-Magic/src/controllers/userController.js
@@ -4,6 +4,19 @@ const userController = Router();
 
 //TODO: Implement user-related routes
 userController.get("/register", (req, res) => {
-    res.send("User registration page");
+    res.render('user/register')
 });
+
+userController.post('/register',async (req, res) => {
+    const { email, password, rePassword } = req.body;
+   
+    if (!email || !password || !rePassword) {
+        return res.status(400).render('user/register', { error: 'All fields are required.' });
+    }
+    if (password !== rePassword) {
+        return res.status(400).render('user/register', { error: 'Passwords do not match.' });
+    }
+    await userService.register({ email, password });
+    res.redirect('/users/login');
+})
 export default userController;
