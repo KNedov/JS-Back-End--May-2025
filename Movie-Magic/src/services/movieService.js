aa7f62b80b425a144adb8aef3c0c7efa20da3170
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-06-15 11:54:10 +0300

    add Register only unique emails

--- Movie-Magic/src/services/movieService.js
+++ Movie-Magic/src/services/movieService.js
@@ -1,19 +1,17 @@
 import { v4 as uuid } from "uuid";
 
 import Movie from "../models/Movie.js";
-// import Cast from "../models/Cast.js";
 
 export default {
     async getAll(filter = {}) {
         let query = Movie.find();
-        // let result = await Movie.find({}).lean();
+
         if (filter.search) {
             query = query.find({
                 title: { $regex: new RegExp(filter.search, "i") },
             });
         }
         if (filter.genre) {
-            // result=result.filter(m=>m.genre.toLowerCase()===filter.genre.toLowerCase());
             query = query.find({ genre: filter.genre.toLowerCase() });
         }
         if (filter.year) {
@@ -25,11 +23,8 @@ export default {
         const movie = new Movie(movieData);
         movie.owner = userId;
 
-        //set owner
-
-        //convert rating to number
         movieData.rating = Number(movieData.rating);
-        //convert year to number
+
         movieData.year = Number(movieData.year);
 
         return movie.save();
@@ -45,17 +40,10 @@ export default {
         return movie.save();
     },
     async deleteMovie(movieId) {
-  
         return Movie.findByIdAndDelete(movieId);
         await Movie.delete(movieId);
     },
     async editMovie(movieId, movieData) {
-       return Movie.findByIdAndUpdate(movieId, movieData)}
-    // async getCasts(movieId) {
-    //     const movie = await this.getOne(movieId);
-
-    //     const casts= await Cast.find({_id:{$in:movie.casts}})
-    //     return casts;
-    // }
+        return Movie.findByIdAndUpdate(movieId, movieData);
+    },
 };
-    
